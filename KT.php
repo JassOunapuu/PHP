8adb7e8a99272079bb6f2c4259b1bd6fddffaa7a
JassOunapuu <98941717+JassOunapuu@users.noreply.github.com>
2023-03-09 11:24:05 +0200

    Add files via upload

---
+++ KT.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <title>Harjutus</title>
+</head>
+
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+		<a class="navbar-brand" href="#">Onapager</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item active">
+					<a class="nav-link" href="index.php">Avaleht</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="leht1.php">Leht 1</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="leht2.php">Leht 2</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="leht3.php">Leht 3</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container">
+		<h1>Avaleht</h1>
+		<p>veebileht.</p>
+	</div>
+	
+	<!-- Bootstrap JavaScript -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+
+
+
+
+
+
+<?php
+/* Jass Õunapuu 09.03.23 */
+
+
+
+
+?>
+
+
+
+</body>
+</html>
